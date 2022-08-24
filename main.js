eaf04d6349ffec7737f299cd92e121c44f4549d9
Markus <markus.h.jarvis@gmail.com>
2022-08-24 10:06:37 -0500

    Add book item shadows and additional javascript notes

--- main.js
+++ main.js
@@ -95,75 +95,37 @@ function displayLibrary() {
 
         /* Add event listener with button to remove book */
         button.addEventListener("click", function() {
-            console.log(button.value);
-
-            myLibrary = JSON.parse(localStorage.getItem("myLibrary"))
-
-            removeBookFromLibrary(button.value);
-
-            localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-
+            myLibrary = JSON.parse(localStorage.getItem("myLibrary"))       // retrieve object
+            removeBookFromLibrary(button.value);                            // remove book function
+            localStorage.setItem("myLibrary", JSON.stringify(myLibrary));   // save object
         })
 
-        bookContainer.appendChild(button);
+        bookContainer.appendChild(button);      // append button to each book div
     }
 }
 
 displayLibrary();
 
-/********* Event Listeners To Add/Remove Buttons *********/
-
-addButton.addEventListener("click", function() {
+/* Event listeners for form controls */
+addButton.addEventListener("click", function() {        // displays form
     formContainer.style.visibility = 'visible';
 });
-removeButton.addEventListener("click", function() {
+
+removeButton.addEventListener("click", function() {     // not needed (DELETE)
     formContainer.style.visibility = 'hidden';
 });
 
-submitButton.addEventListener("click", function() {
+submitButton.addEventListener("click", function() {     // submits form and creates book
     addBookToLibrary(title.value, author.value, pages.value, read.checked);
 })
 
 
 
 
-
-/*********
-
-/* const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 1080, 'read');
-const mobyDick = new Book('Moby Dick', 'Herman Melvile', 794, 'not read')
-myLibrary.push(theHobbit);
-myLibrary.push(mobyDick);
-
-console.log(myLibrary[0].title);
-console.log(theHobbit.info());
-console.log(mobyDick.info()); */
-
-/* Create Form Element */
-/* const main_container = document.querySelector('.main-container');
-
-const div = document.createElement('div');
-div.classList.add('book');
-div.id = 'box-0';
-
-main_container.appendChild(div);
- */
-
-
-
 /*
-
-1. Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-
-2. Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
-3. Add a button on each book’s display to remove the book from the library.
-
-4. You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
 5. Add a button on each book’s display to change its read status.
 
 6. To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
-
-7. NOTE: You’re not required to add any type of storage right now. You will have the option to come back to this project later on in the course. 
 
 */
