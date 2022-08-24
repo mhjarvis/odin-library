766b8b8668a7c2a942c9958aa3b0cb4c47f1cf8d
Markus <markus.h.jarvis@gmail.com>
2022-08-24 09:57:11 -0500

    Add notes to javascript for claity; slight restructuring for readability

--- main.js
+++ main.js
@@ -1,102 +1,99 @@
-
+/* Initialize myLibrary array or retrieve it from the browser if a version was previously stored */
 let myLibrary = JSON.parse(localStorage.getItem("myLibrary")) || [];
-console.log(myLibrary);
 
-
-function Book(title, author, pages, read) {           // constructor
+/* Constructor for all Book objects */
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-Book.prototype.info = function () {     // make functions protos so they are not created for every object
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-}
-
-
+/* Function to add new book to library; save to storage after adding */
 function addBookToLibrary(title, author, pages, read) {
-    if(read) {
+
+    if(read) {                  // convert true/false values to readable input for user
         read = 'Read';
     } else {
         read = 'Not Read';
     }
-    const temp = new Book(title, author, pages, read);
-    myLibrary.push(temp);
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+
+    const book = new Book(title, author, pages, read);                  // create new book
+    myLibrary.push(book);                                               // add to myLibrary arr
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));       // save to localStorage
 }
 
-function removeBookFromLibrary(val) {
-    myLibrary.splice(val, 1);
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    displayLibrary();
+/* Function to remove book from the library */
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);                                         // remove selected item from arr
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));       // save new array to storage
+    displayLibrary();                                                   // rebuild book container
 }
 
+/* HTML query selectors to be reused */
+const addButton = document.querySelector('.add-book');                  // header div
+const removeButton = document.querySelector('.remove-book');            // header div
+const formContainer = document.querySelector('.form-container');        // form element
+const submitButton = document.querySelector('#submit-button');          // form button
+const removeBookButton = document.querySelector('.remove-button');      // book remove button
+const title = document.querySelector('#the_title');                     // book title
+const author = document.querySelector('#the_author');                   // book author
+const pages = document.querySelector('#the_pages');                     // book page count
+const read = document.querySelector('#checkbox_read');                  // book read status
+const mainContainer = document.querySelector('.book-container');        // main body div holding all books
 
-const addButton = document.querySelector('.add-book');                  // + button in header
-const removeButton = document.querySelector('.remove-book');            // - button in header
-const formContainer = document.querySelector('.form-container');        // main container
-const submitButton = document.querySelector('#submit-button');
-const removeBookButton = document.querySelector('.remove-button');
 
-const title = document.querySelector('#the_title');
-const author = document.querySelector('#the_author');
-const pages = document.querySelector('#the_pages');
-const read = document.querySelector('#checkbox_read');
-
-
-/********* Display Library on Page *********/
-const mainContainer = document.querySelector('.book-container');
-
+/* Display library and/or rebuild library */
 function displayLibrary() {
-    mainContainer.innerHTML = '';
-    JSON.parse(localStorage.getItem("myLibrary"))
-    for(let i = 0; i < myLibrary.length; i++) {             // loop through myLibrary array
+    mainContainer.innerHTML = '';                                   // clear html container
+    JSON.parse(localStorage.getItem("myLibrary"))                   // retrieve myLibrary arr
+    for(let i = 0; i < myLibrary.length; i++) {                     // loop through myLibrary arr
 
-        // Add book container to main element
+        /* Add individual book container to main element*/
         const div = document.createElement('div');
         div.classList.add('book');
         div.id = `book-${i}`;
         mainContainer.appendChild(div);
 
-        // Add const variables for creating individual divs
+        /* Primary element for adding books to */
         const bookContainer = document.getElementById(`book-${i}`);
-        const titleDiv = document.createElement('div');
-        const authorDiv = document.createElement('div');
-        const pagesDiv = document.createElement('div');
-        const readDiv = document.createElement('div');
-        const button = document.createElement('button');
 
-        // Create title div and add to book container
+        /* Create title div and add to book container */
+        const titleDiv = document.createElement('div');
         titleDiv.classList.add('title-div');
         titleDiv.classList.add('book-info')
         titleDiv.innerHTML = myLibrary[i].title;
         bookContainer.appendChild(titleDiv);
 
-        // Create author div and add to book container
+        /* Create author div and add to book container */
+        const authorDiv = document.createElement('div');
         authorDiv.classList.add('author-div');
         authorDiv.classList.add('book-info');
         authorDiv.innerHTML = myLibrary[i].author;
         bookContainer.appendChild(authorDiv);
 
-        // Create pages div and add to book container
+        /* Create pages div and add to book container */
+        const pagesDiv = document.createElement('div');
         pagesDiv.classList.add('pages-div');
         pagesDiv.classList.add('book-info')
         pagesDiv.innerHTML = myLibrary[i].pages;
         bookContainer.appendChild(pagesDiv);
 
-        // Create read div and add to book container
+        /* Create read div and add to book container */
+        const readDiv = document.createElement('div');
         readDiv.classList.add('read-div');
         readDiv.classList.add('book-info')
         readDiv.innerHTML = myLibrary[i].read;
         bookContainer.appendChild(readDiv);
 
-        // Create remove book button
+        /* Create remove book button */
+        const button = document.createElement('button');
         button.classList.add('remove-button');
         button.id = 'delete-book-button' + i;
         button.innerHTML = 'Remove';
         button.value = i;
 
+        /* Add event listener with button to remove book */
         button.addEventListener("click", function() {
             console.log(button.value);
 
